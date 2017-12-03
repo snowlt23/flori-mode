8c827410ff34970e8ff213e653818cc5fa596c3c
snowlt23 <shsnow23@gmail.com>
2017-12-03 21:18:26 +0900

    fix indentation

--- flori-mode.el
+++ flori-mode.el
@@ -2,9 +2,10 @@
 (setq flori-ident "\\(\\([a-z]\\|[A-Z]\\)\\([a-z]\\|[A-Z]\\|[0-9]\\|_\\)*?\\)")
 (setq flori-separates "\\((\\|)\\|[\\|]\\|\s\\|\n\\|\r\\|,\\)")
 
-(setq flori-keywords '("match" "if" "elif" "else" "cond" "for" "while" "require"))
+(setq flori-keywords '("match" "if" "elif" "else" "cond" "for" "while" "import" "destructor" "return"))
 
 (setq flori-keywords-regexp (regexp-opt flori-keywords 'words))
+(setq flori-def-regexp (concat flori-ident "\s*:=\s*"))
 (setq flori-defn-regexp (concat "\\(fn\\)\s" "\\(.+?\\)" flori-separates))
 (setq flori-deftype-regexp (concat "\\(type\\)\s" "\\(.+?\\)" flori-separates))
 (setq flori-type-regexp (concat "\\([A-Z]\\([a-z]\\|[A-Z]\\|[0-9]\\)*?\\)" flori-separates))
@@ -14,6 +15,7 @@
 
 (setq flori-font-lock-keywords
       `((,flori-keywords-regexp . font-lock-keyword-face)
+        (,flori-deftype-regexp (1 font-lock-variable-name-face))
         (,flori-defn-regexp (1 font-lock-keyword-face) (2 font-lock-function-name-face))
         (,flori-deftype-regexp (1 font-lock-keyword-face) (2 font-lock-function-name-face))
         (,flori-type-regexp (1 font-lock-type-face))
@@ -22,15 +24,31 @@
         (,flori-string-regexp . font-lock-string-face)))
 
 (defun flori-indent-line ()
+  (interactive)
+  (beginning-of-line)
+  (if (eq cur-indent nil)
+      (setq cur-indent 0))
   (save-excursion
-    (beginning-of-line)
-    (indent-line-to (* 2 (car (syntax-ppss)))))
-  (forward-char (* 2 (car (syntax-ppss)))))
+    (if (looking-at "^[ \t]*\}")
+        (indent-line-to (* 2 (- (car (syntax-ppss)) 1)))
+      (indent-line-to (* 2 (car (syntax-ppss))))))
+  (back-to-indentation))
 (defun flori-indent-region (beg end)
+  (interactive)
   (indent-region beg end nil))
 
+(defvar flori-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" st)
+    (modify-syntax-entry ?\" "\"" st)
+    (modify-syntax-entry ?# "<" st)
+    (modify-syntax-entry ?\n "> st")
+    (modify-syntax-entry ?\{ "(")
+    (modify-syntax-entry ?\} ")")
+    st))
+
 (define-derived-mode flori-mode fundamental-mode "Flori"
-  :syntax-table emacs-lisp-mode-syntax-table
+  :syntax-table flori-mode-syntax-table
   (setq indent-tabs-mode nil)
   (setq indent-line-function #'flori-indent-line)
   (setq comment-start ";;")
